b45a2f241c8f4eb9348b7c6ef9de299d316e4afb
arthurlerke <Arthur.Lerke@Student.HTW-Berlin.de>
2019-11-08 14:51:54 +0100

    Replaced default encoder with BCrypt

--- src/main/java/de/htw/foodnet/config/WebSecurityConfig.java
+++ src/main/java/de/htw/foodnet/config/WebSecurityConfig.java
@@ -1,4 +1,51 @@
 package de.htw.foodnet.config;
 
-public class WebSecurity extends  {
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/about").hasRole("CHEF")
+                .antMatchers("/recipes/").hasAnyRole()
+                .antMatchers("/recipes/new").hasRole("CHEF")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin();
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        UserDetails student =
+                User.builder()
+                        .username("student")
+                        .password(passwordEncoder().encode("password"))
+                        .roles("STUDENT")
+                        .build();
+        UserDetails chef =
+                User.builder()
+                        .username("chef")
+                        .password(passwordEncoder().encode("chef"))
+                        .roles("CHEF")
+                        .build();
+        return new InMemoryUserDetailsManager(student,chef);
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
