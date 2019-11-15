039a4f3dc23eb37883d312da44317e30b34c9edf
eelok <eelok@mail.ru>
2019-11-15 14:56:13 +0100

    fix createRecipesController -> CreateRecipesController

--- src/main/java/de/htw/foodnet/recipes/createNew/CreateRecipesController.java
+++ src/main/java/de/htw/foodnet/recipes/createNew/CreateRecipesController.java
@@ -1,11 +1,11 @@
-package de.htw.foodnet.Controller;
+package de.htw.foodnet.recipes.createNew;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class createRecipesController {
+public class CreateRecipesController {
     @RequestMapping(value="createRecipes")
     public ModelAndView createRecipes() {
         return new ModelAndView("createRecipes");
