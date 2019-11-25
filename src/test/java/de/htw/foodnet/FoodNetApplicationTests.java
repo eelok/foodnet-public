57a23029d70f5eb43cdca03977aca8097852def5
arthurlerke <Arthur.Lerke@Student.HTW-Berlin.de>
2019-11-25 15:48:29 +0100

    Trying to fix Travis

--- src/test/java/de/htw/foodnet/FoodNetApplicationTests.java
+++ src/test/java/de/htw/foodnet/FoodNetApplicationTests.java
@@ -3,12 +3,14 @@ package de.htw.foodnet;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class FoodNetApplicationTests {
 
     @Test
     void contextLoads() {
-
+        assertTrue(true);
     }
 
 }
