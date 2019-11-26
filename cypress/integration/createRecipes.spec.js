6cba83fa3404ed279501f08ec161c99b191327f5
eelok <eelok@mail.ru>
2019-11-26 19:53:02 +0100

    add cypress test for new Recipes

--- cypress/integration/createRecipes.spec.js
+++ cypress/integration/createRecipes.spec.js
@@ -1,9 +1,22 @@
-describe('About us Test', function () {
-    beforeEach(function () {
-        cy.visit('localhost:8080/createRecipes');
-    });
-    it('Get Title', function () {
-        cy.title().should('eq', 'Create Recipes');
-    });
+function login() {
+    cy.get('input[name=username]').type("chef");
+    cy.get('input[name=password]').type('chef');
+    cy.get('.btn').contains("Sign in").click();
+}
 
+describe('new Recipe Page Test', function () {
+    beforeEach(function () {
+        cy.visit('http://localhost:8080/recipes/new');
+        login();
+    });
+    it('Test Create Recipe', function () {
+        cy.get('input[id=recipe_name]').type("pasta pesto rosso");
+        cy.get('input[id=prepare_time]').clear().type("13");
+        cy.get('input[id=cook_time]').clear().type("20");
+        cy.get('input[id=ingredients]').type("mushrooms:40, avocado:20, carrot:30");
+        cy.get('textarea[id=description]').type(
+            "There's nothing like the taste of a cake you made in your own kitchen.");
+        cy.get('.btn').contains('Create Recipe').click();
+        cy.get('.message.message--success').should('be.visible');
+    });
 });
