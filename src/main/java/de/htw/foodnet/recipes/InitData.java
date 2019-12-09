07dea95ff8871ec0c7698b14d91dcc5a5c5971bb
eelok <eelok@mail.ru>
2019-12-09 20:47:54 +0100

    use stream

--- src/main/java/de/htw/foodnet/recipes/InitData.java
+++ src/main/java/de/htw/foodnet/recipes/InitData.java
@@ -4,7 +4,9 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
@@ -101,7 +103,17 @@ public class InitData {
     }
 
     private byte[] extractBytes(String imageName) throws IOException {
-        return Files.readAllBytes(Paths.get(this.getClass().getResource(imageName).getPath()));
+        InputStream resourceAsStream = this.getClass().getResourceAsStream(imageName);
+        return getBytesFromInputStream(resourceAsStream);
+    }
+
+    public static byte[] getBytesFromInputStream(InputStream is) throws IOException {
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        byte[] buffer = new byte[0xFFFF];
+        for (int len = is.read(buffer); len != -1; len = is.read(buffer)) {
+            os.write(buffer, 0, len);
+        }
+        return os.toByteArray();
     }
 
 }
