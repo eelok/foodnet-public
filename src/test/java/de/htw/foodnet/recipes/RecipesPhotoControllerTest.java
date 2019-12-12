d17ddc17709847a4852be581ad7f5ec534f480ba
Tim Tkachenko <tim.tkachenko@idealo.de>
2019-12-12 16:49:43 +0100

    add tests for RecipesPhotoController

---
+++ src/test/java/de/htw/foodnet/recipes/RecipesPhotoControllerTest.java
@@ -0,0 +1,65 @@
+package de.htw.foodnet.recipes;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class RecipesPhotoControllerTest {
+
+    private RecipesPhotoController recipesPhotoController;
+    @Mock
+    private RecipePhotoService recipePhotoService;
+
+    @BeforeEach
+    void setUp() {
+        recipesPhotoController = new RecipesPhotoController(recipePhotoService);
+    }
+
+    @Test
+    void should_getRecipePhoto() {
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        long photoId = 5;
+
+        recipesPhotoController.getRecipePhoto(photoId, request, response);
+
+        verify(recipePhotoService).findByRecipeId(photoId);
+    }
+
+    @Test
+    void should_setContentType() throws IOException {
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        long photoId = 5;
+
+        ImageFile imageFile = mock(ImageFile.class);
+        when(imageFile.getContentType()).thenReturn("contentType");
+        byte[] byteArr = new byte[] {1};
+        when(imageFile.getContent()).thenReturn(byteArr);
+        when(imageFile.getContentLength()).thenReturn(20);
+        Optional<ImageFile> image = Optional.of(imageFile);
+
+        when(recipePhotoService.findByRecipeId(photoId)).thenReturn(image);
+        ServletOutputStream out = mock(ServletOutputStream.class);
+        when(response.getOutputStream()).thenReturn(out);
+
+        recipesPhotoController.getRecipePhoto(photoId, request, response);
+
+        verify(response).setContentType(imageFile.getContentType());
+        verify(response).setContentLength(imageFile.getContentLength());
+    }
+
+
+}
